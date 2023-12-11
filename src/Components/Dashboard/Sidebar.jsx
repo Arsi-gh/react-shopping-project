39f9fc1ebaead2295ff642be3917bb902d3ec860
Arsi <arsalanghoochani15@gmail.com>
2023-12-11 12:12:36 -0800

    React-shopping-project-v-1.3

--- src/Components/Dashboard/Sidebar.jsx
+++ src/Components/Dashboard/Sidebar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 export default function Sidebar() {
 
-  const [role , setRole] = useState(false)
+  const [role , setRole] = useState(true)
 
   return (
     <section className='relative'>
